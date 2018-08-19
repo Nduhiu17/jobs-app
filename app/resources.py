c576eaec514127cac9aa0457014e05f8b4e6c468
Your Name <nduhiu254@gmail.com>
2018-08-20 00:12:46 +0300

    Add test

--- app/resources.py
+++ app/resources.py
@@ -2,9 +2,7 @@ from flask_restplus import Resource, reqparse
 from flask import jsonify
 from app import api
 from app.models import Job
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required,
-                                get_jwt_identity, get_raw_jwt)
-from app.schemas import job_schema, jobs_schema
+from app.schemas import jobs_schema
 
 parser = reqparse.RequestParser()
 parser.add_argument('title', help='This field cannot be blank', required=True)
