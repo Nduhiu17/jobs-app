d40a783f8dfeb22fee62f9963dfa4ffbe74b896e
Your Name <nduhiu254@gmail.com>
2018-08-21 00:58:39 +0300

    Add procfile and set up heroku postgres database uri

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source virtual/bin/activate
 export SECRET_KEY='hii-inafaa-kua-siri'
-export DATABASE_URL='postgresql://nduhiu:password@localhost:5432/jobs_db'
+export DATABASE_URL='postgres://ufckukzsbxdthh:da699e41a8ba339d4b9e19ff96cc3c2a22b0893a12afcc3d318f1cd51fa8605a@ec2-184-73-202-79.compute-1.amazonaws.com:5432/dd3tkdla7hip96'
 export ENVIROMENT='testing'
 export  CC_TEST_REPORTER_ID='887ac3d0f3c60fc2d5f4c0b85489f1982a53861a56dc35a892188264708c38a6'
 python3.6 manage.py server
